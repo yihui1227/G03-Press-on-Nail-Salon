7293b57aadb548ffb4c6286431f41d7f16773957
YIHUI <112804328+yihui1227@users.noreply.github.com>
2024-12-30 02:58:39 +0800

    login

--- src/condb.php
+++ src/condb.php
@@ -1,11 +1,11 @@
 <?php
     $user = 'root'; //資料庫使用者名稱
-    $password = '0726'; //資料庫的密碼
+    $password = 'yihui888'; //資料庫的密碼
     try {
         $db = new PDO ('mysql:host=localhost;dbname=g03;charset=utf8', $user, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        echo "資料庫連線成功！";
+        //echo "資料庫連線成功！";
     } catch(PDOException $e) {
         echo "資料庫連線失敗：" . $e->getMessage();
     }
--- src/login_process.php
+++ src/login_process.php
@@ -3,11 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    die('Method Not Allowed');
-}
-
 // 帳碼
 $correct_username = 'admin';
 $correct_password = 'admin';
@@ -16,7 +11,8 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 if ($username === $correct_username && $password === $correct_password) {
-    header('Location: ../dashboard.php');
+    $_SESSION['admin_logged_in'] = true;
+    header('Location: ../src/dashboard.php');
     exit();
 } else {
     header('Location: ../assets/login.html?error=1');
